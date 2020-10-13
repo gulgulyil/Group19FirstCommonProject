136a2b28e084f5cbf5594f0614f7975c509364c9
EmreCihanbeyoglu <mrcihanbeyoglu@gmail.com>
2020-10-13 20:51:27 +0200

    initial commit before sharing to all group members

---
+++ src/test/java/FirstClassBeforeShare.java
@@ -0,0 +1,6 @@
+public class FirstClassBeforeShare {
+    public static void main(String[] args) {
+        System.out.println("Hi everyone!!");
+        //this file was created before sharing to all of you. First class.
+    }
+}
